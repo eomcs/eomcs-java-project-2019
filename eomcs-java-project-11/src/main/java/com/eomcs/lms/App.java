8dac78df4c16fc917143916b084462f2dbc5d39a
Jinyoung Eom <jinyoung.eom@gmail.com>
2018-12-21 12:36:31 +0900

    11.패키지로 클래스를 분류하기

---
+++ eomcs-java-project-11/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,58 @@
+package com.eomcs.lms;
+
+import java.util.Scanner;
+import com.eomcs.lms.handler.BoardHandler;
+import com.eomcs.lms.handler.LessonHandler;
+import com.eomcs.lms.handler.MemberHandler;
+
+public class App {
+
+  static Scanner keyboard = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    
+    // 외부로 분리한 클래스에서 사용할 keyboard를 주입한다.
+    LessonHandler.keyboard = keyboard;
+    MemberHandler.keyboard = keyboard;
+    BoardHandler.keyboard = keyboard;
+    
+    while (true) {
+      String command = prompt();
+
+      if (command.equals("/lesson/add")) {
+        LessonHandler.addLesson();
+        
+      } else if (command.equals("/lesson/list")) {
+        LessonHandler.listLesson();
+      
+      } else if (command.equals("/member/add")) {
+        MemberHandler.addMember();
+        
+      } else if (command.equals("/member/list")) {
+        MemberHandler.listMember();
+        
+      } else if (command.equals("/board/add")) {
+        BoardHandler.addBoard();
+        
+      } else if (command.equals("/board/list")) {
+        BoardHandler.listBoard();
+        
+      } else if (command.equals("quit")) {
+        System.out.println("안녕!");
+        break;
+        
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
+      
+      System.out.println(); // 결과 출력 후 빈 줄 출력
+    }
+
+    keyboard.close();
+  }
+
+  private static String prompt() {
+    System.out.print("명령> ");
+    return keyboard.nextLine().toLowerCase();
+  }
+}
