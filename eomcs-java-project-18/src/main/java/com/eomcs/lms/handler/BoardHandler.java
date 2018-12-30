452f946153948b4bd72cf9a69d232b0231e753bd
Jinyoung Eom <jinyoung.eom@gmail.com>
2018-12-30 22:47:09 +0900

    18.제네릭을 사용하는 방법과 이점

---
+++ eomcs-java-project-18/src/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -0,0 +1,44 @@
+package com.eomcs.lms.handler;
+import java.sql.Date;
+import java.util.Scanner;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.util.ArrayList;
+
+public class BoardHandler {
+  
+  Scanner keyboard;
+  ArrayList<Board> list;
+  
+  public BoardHandler(Scanner keyboard) {
+    this.keyboard = keyboard;
+    this.list = new ArrayList<>(20);
+  }
+  
+  public void listBoard() {
+    Board[] boards = list.toArray(new Board[] {});
+    for (Board board : boards) {
+      System.out.printf("%3d, %-20s, %s, %d\n", 
+          board.getNo(), board.getContents(), 
+          board.getCreatedDate(), board.getViewCount());
+    }
+  }
+
+  public void addBoard() {
+    Board board = new Board();
+    
+    System.out.print("번호? ");
+    board.setNo(Integer.parseInt(keyboard.nextLine()));
+    
+    System.out.print("내용? ");
+    board.setContents(keyboard.nextLine());
+    
+    board.setCreatedDate(new Date(System.currentTimeMillis())); 
+    
+    board.setViewCount(0);
+    
+    list.add(board);
+    
+    System.out.println("저장하였습니다.");
+  }
+
+}
---
+++ eomcs-java-project-18/src/main/java/com/eomcs/util/ArrayList.java
@@ -0,0 +1,41 @@
+package com.eomcs.util;
+
+import java.util.Arrays;
+
+public class ArrayList<E> {
+  final int DEFAULT_CAPACITY = 10;
+  Object[] list;
+  int size = 0;
+  
+  public ArrayList() {
+    list  = new Object[DEFAULT_CAPACITY];
+  }
+  
+  public ArrayList(int initialCapacity) {
+    if (initialCapacity > DEFAULT_CAPACITY) 
+      list = new Object[initialCapacity];
+    else
+      list = new Object[DEFAULT_CAPACITY];
+  }
+  
+  @SuppressWarnings("unchecked")
+  public E[] toArray(E[] a) {
+    if (a.length < size) {
+      return (E[]) Arrays.copyOf(list, size, a.getClass());
+    }
+    System.arraycopy(list, 0, a, 0, size);
+    if (a.length > size)
+      a[size] = null;
+    return a;
+  }
+  
+  public void add(E obj) {
+    if (size >= list.length) {
+      int oldCapacity = list.length;
+      int newCapacity = oldCapacity + (oldCapacity >> 1);
+      list = Arrays.copyOf(list, newCapacity);
+    }
+    
+    list[size++] = obj;
+  }
+}
